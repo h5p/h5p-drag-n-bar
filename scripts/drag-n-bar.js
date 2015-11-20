84f07d3c1f04f490891a8a1cb735b5c22caabe62
Thomas Marstrander <thomasmars87@gmail.com>
2015-11-20 09:29:26 +0100

    Fixed for forcing redraw on dnb container using hardware acceleration.  Fixed an issue where context menu would be drawn outside dnb container then moved to its correct place. [HFJ-1350]

--- scripts/drag-n-bar.js
+++ scripts/drag-n-bar.js
@@ -38,6 +38,9 @@ H5P.DragNBar = (function (EventDispatcher) {
     // Create a popup dialog
     this.dialog = new H5P.DragNBarDialog($dialogContainer, $container);
 
+    // Fix for forcing redraw on $container, to avoid "artifcats" on safari
+    this.$container.addClass('hardware-accelerated');
+
     if (this.isEditor) {
       this.initEditor();
       this.initClickListeners();
@@ -498,6 +501,7 @@ H5P.DragNBar.prototype.focus = function ($element) {
   if (this.focusedElement) {
     this.focusedElement.showContextMenu();
     this.focusedElement.focus();
+    self.updateCoordinates();
   }
 
   // Wait for potential recreation of element
