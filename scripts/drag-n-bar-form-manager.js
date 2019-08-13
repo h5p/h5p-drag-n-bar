403d411f98c0b3a5e84872d9e9b30c851288b11f
Thomas Marstrander <marstranderthomas@gmail.com>
2019-08-13 11:24:40 +0200

    HFP-2819 Add footer in form manager

--- scripts/drag-n-bar-form-manager.js
+++ scripts/drag-n-bar-form-manager.js
@@ -17,7 +17,7 @@
     EventDispatcher.call(self);
 
     const formTargets = [self];
-    let head, subForm, titles, handleTransitionend, proceedButton, breadcrumbButton;
+    let head, footer, footerProceedButton, subForm, titles, handleTransitionend, proceedButton, breadcrumbButton;
 
     /**
      * Initialize the FormManager.
@@ -30,9 +30,14 @@
       // Locate target container
       self.formContainer = (parent instanceof H5PEditor.Library ? parent.$libraryWrapper : parent.$form)[0];
       self.formContainer.classList.add('form-manager');
+      self.formContainer.classList.add('root-form');
 
       head = document.createElement('div');
       head.classList.add('form-manager-head');
+      footer = document.createElement('div');
+      footer.classList.add('form-manager-head');
+      footer.classList.add('form-manager-footer');
+      self.footer = footer;
 
       // Create button to toggle preivous menu on narrow layouts
       breadcrumbButton = createButton('breadcrumb-menu', l10n.expandBreadcrumbButtonLabel, self.toggleBreadcrumbMenu);
@@ -64,12 +69,25 @@
         }
       });
       hideElement(proceedButton);
+      footerProceedButton = createButton('proceed', l10n.proceedButtonLabel, function () {
+        if (manager.exitSemiFullscreen) {
+          // Trigger semi-fullscreen exit
+          manager.exitSemiFullscreen();
+          manager.exitSemiFullscreen = null;
+        }
+      });
+      hideElement(footerProceedButton);
+      footer.appendChild(footerProceedButton);
       head.appendChild(proceedButton);
 
       // Create a container for the action buttons
       self.formButtons = document.createElement('div');
       self.formButtons.classList.add('form-manager-buttons');
+      self.footerFormButtons = document.createElement('div');
+      self.footerFormButtons.classList.add('form-manager-buttons');
+      hideElement(self.footerFormButtons);
       hideElement(self.formButtons); // Buttons are hidden by default
+      footer.appendChild(self.footerFormButtons);
       head.appendChild(self.formButtons);
 
       // Create 'Delete' button
@@ -85,6 +103,20 @@
       self.formButtons.appendChild(createButton('done', l10n.doneButtonLabel, function () {
         formTargets[formTargets.length - 1].trigger('formdone');
         closeForm();
+      }));
+
+      // Footer form buttons
+      self.footerFormButtons.appendChild(createButton('done', l10n.doneButtonLabel, function () {
+        formTargets[formTargets.length - 1].trigger('formdone');
+        closeForm();
+      }));
+
+      self.footerFormButtons.appendChild(createButton('delete', l10n.deleteButtonLabel, function () {
+        const e = new H5P.Event('formremove');
+        formTargets[formTargets.length - 1].trigger(e);
+        if (!e.preventRemove) {
+          closeForm();
+        }
       }));
 
       // Check if we can has fullscreen
@@ -129,6 +161,9 @@
 
       // Insert everything in the top of the form DOM
       self.formContainer.insertBefore(head, self.formContainer.firstChild);
+      hideElement(footer);
+      self.formContainer.appendChild(manager.footer);
+
 
       // Always clean up on remove
       self.on('validate', function () {
@@ -400,6 +435,13 @@
         // No need for the buttons any more
         hideElement(manager.formButtons);
         manager.formButtons.classList.remove('form-manager-comein');
+
+        // Hide footer
+        manager.footerFormButtons.classList.remove('form-manager-comein');
+        hideElement(manager.footerFormButtons);
+        hideElement(manager.footer);
+
+        manager.formContainer.classList.add('root-form');
       }
 
       // Animation fix for fullscreen max-width limit.
@@ -447,6 +489,9 @@
       if (self.formContainer.classList.contains('mobile-menu-open')) {
         self.toggleBreadcrumbMenu();
       }
+
+      // Scroll parent manager header into view
+      manager.formButtons.scrollIntoView();
     };
 
     /**
@@ -538,6 +583,7 @@
       }
 
       // Tell manager that we should be receiving the next buttons events
+      manager.formContainer.classList.remove('root-form');
       manager.addFormTarget(self);
 
       // Create the new sub-form
@@ -569,15 +615,23 @@
 
       // Show our buttons
       showElement(manager.formButtons);
+      showElement(manager.footerFormButtons);
+      showElement(manager.footer);
+
+      // Ensure footer is at the bottom of the form
+      manager.formContainer.appendChild(manager.footer);
 
       // When transition animation is done and the form is fully open...
       handleTransitionend = onlyOnce(subForm, 'transitionend', function () {
         handleTransitionend = null;
 
-        // Hide everything except first, second & last child
+        // Hide everything except first, second, last child and footer
         for (let i = 2; i < manager.formContainer.children.length - 1; i++) {
           const child = manager.formContainer.children[i];
-          if (child !== subForm && !child.classList.contains('sp-container')) {
+          const skipHiding = child === subForm
+            || child.classList.contains('sp-container')
+            || child.classList.contains('form-manager-footer');
+          if (!skipHiding) {
             hideElement(manager.formContainer.children[i]);
           }
         }
@@ -598,6 +652,7 @@
         subForm.classList.add('form-manager-slidein');
         titles.breadcrumb.classList.add('form-manager-comein');
         manager.formButtons.classList.add('form-manager-comein');
+        manager.footerFormButtons.classList.add('form-manager-comein');
         manager.updateFormResponsiveness();
       }, 0);
 
